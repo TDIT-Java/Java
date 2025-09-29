7bdd1b8734012f9608eed837b431938288ad13b6
Pradeep <pradeepparit2001@gmail.com>
2025-09-29 13:54:12 +0530

    InstanceInheritance.java added

---
+++ Java/src/Pradeep/staticAncInstanceBlock/instanceBlock/InstanceInheritance.java
@@ -0,0 +1,16 @@
+package Pradeep.staticAncInstanceBlock.instanceBlock;
+
+public class InstanceInheritance {
+    int a = 10;
+
+    {
+        int b = 20;
+        System.out.println("instance block 1");
+    }
+}
+
+class test extends InstanceInheritance {
+    public static void main(String[] args) {
+        test obj = new test();
+    }
+}
