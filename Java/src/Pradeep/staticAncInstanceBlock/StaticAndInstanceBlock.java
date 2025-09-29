c87fc1a409f3d5262953e3c6af6ea6145f3888b1
Pradeep <pradeepparit2001@gmail.com>
2025-09-29 13:36:49 +0530

    blocks example

---
+++ Java/src/Pradeep/staticAncInstanceBlock/StaticAndInstanceBlock.java
@@ -0,0 +1,37 @@
+package Pradeep.staticAncInstanceBlock;
+
+public class StaticAndInstanceBlock extends abc{
+    static{
+        new abc("from constructor block abc");
+        int a = 10;
+        System.out.println("static block 1: " + a);
+    }
+
+    StaticAndInstanceBlock() {
+        super("from super");
+        new abc("from constructor block abc");
+        System.out.println("Constructor");
+    }
+
+    {
+        new abc("from constructor block abc");
+        System.out.println("non-static block 1");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main block");
+        StaticAndInstanceBlock obj = new StaticAndInstanceBlock();
+    }
+}
+
+class abc{
+    abc(String abc){
+        System.out.println(abc);
+    }
+    static{
+        System.out.println("static block abc");
+    }
+    {
+        System.out.println("non-static abc");
+    }
+}
---
+++ Java/src/Pradeep/staticAncInstanceBlock/instanceBlock/InstanceBlock.java
@@ -0,0 +1,24 @@
+package Pradeep.staticAncInstanceBlock.instanceBlock;
+
+public class InstanceBlock {
+    static {
+        new InstanceBlock();
+        System.out.println("static Block");
+    }
+
+    {
+        System.out.println("instance block 1");
+    }
+
+    {
+        System.out.println("instance block 2");
+    }
+
+    InstanceBlock() {
+        System.out.println("Constructor");
+    }
+
+    public static void main(String[] args) {
+         new InstanceBlock();
+    }
+}
