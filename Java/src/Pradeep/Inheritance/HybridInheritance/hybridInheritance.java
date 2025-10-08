f040cc544c902f871a36d5fadcdbbca88ecd66d3
Pradeep <pradeepparit2001@gmail.com>
2025-10-08 14:43:02 +0530

    Implemented Inheritance

---
+++ Java/src/Pradeep/Inheritance/HybridInheritance/hybridInheritance.java
@@ -0,0 +1,43 @@
+package Pradeep.Inheritance.HybridInheritance;
+
+class parent {
+    void method1(){
+        System.out.println("method1");
+    }
+}
+
+class child extends parent{
+    void method2(){
+        System.out.println("method2");
+    }
+}
+
+class child1 extends child{
+    void method3(){
+        System.out.println("method3");
+    }
+}
+
+public class hybridInheritance extends parent{
+    void method4(){
+        System.out.println("method4");
+    }
+
+    public static void main(String[] args) {
+        hybridInheritance obj = new hybridInheritance();
+        obj.method1();
+        obj.method4();
+
+        child1  obj1 = new child1();
+        obj1.method3();
+        obj1.method2();
+        obj1.method3();
+    }
+}
+
+//O/P =>
+//        method1
+//        method4
+//        method3
+//        method2
+//        method3
---
+++ Java/src/Pradeep/Inheritance/MultipleInheritance/MultipleInheritance.java
@@ -0,0 +1,23 @@
+package Pradeep.Inheritance.MultipleInheritance;
+
+class parent {
+    void method()
+    {
+        System.out.println("method1");
+    }
+}
+
+class parent1
+{
+    void  method()
+    {
+        System.out.println("method2");
+    }
+}
+
+//public class MultipleInheritance extends parent, parent1{
+//    public static void main(String[] args) {
+//        MultipleInheritance  obj = new MultipleInheritance();
+//        obj.method();
+//    }
+//}
