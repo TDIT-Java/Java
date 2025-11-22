12758baa8465b52d61ab15cb46d936b1dbb03f9d
Pradeep <pradeepparit2001@gmail.com>
2025-11-22 15:10:19 +0530

    Infinity, -Infinity, NaN

--- Java/src/Pradeep/ExceptionHandling/ExceptionHandling.java
+++ Java/src/Pradeep/ExceptionHandling/ExceptionHandling.java
@@ -15,19 +15,24 @@ public class ExceptionHandling {
 //        }
 //    }
     public static void main(String[] args) {
-        try {
-            System.out.println(10 / 0);
-        } catch (Exception e) {
-//            System.out.println(e.getCause().getMessage());
-            System.out.println(e.getMessage());
-            e.getStackTrace(); // This statement will print first or last
-            // because of System.out and System.err are two different output streams
-//            Their order can sometimes mix depending on the system buffering.
-            System.out.println("Caught Exception");
-            System.out.println(e.toString() + "abc");
-//            e = new IOException(); // We can create a new object of the another exception and throw it also but for that we should have to declare main method throws
-            throw e; // Here we can use sout also and also when we create a new object like above line then it will print null
-//            System.out.println(e.getMessage()); // Cannot write any statement after throw keyword
-        }
+//        try {
+//            System.out.println(10 / 0);
+//        } catch (Exception e) {
+////            System.out.println(e.getCause().getMessage());
+//            System.out.println(e.getMessage());
+//            e.getStackTrace(); // This statement will print first or last
+//            // because of System.out and System.err are two different output streams
+////            Their order can sometimes mix depending on the system buffering.
+//            System.out.println("Caught Exception");
+//            System.out.println(e.toString() + "abc");
+////            e = new IOException(); // We can create a new object of the another exception and throw it also but for that we should have to declare main method throws
+//            throw e; // Here we can use sout also and also when we create a new object like above line then it will print null
+////            System.out.println(e.getMessage()); // Cannot write any statement after throw keyword
+//        }
+
+//        System.out.println(10.0/0); // will product Infinity
+//        System.out.println(-10.0/0); // will product -Infinity
+//        System.out.println(0.0/0); // will product NaN
+
     }
 }
