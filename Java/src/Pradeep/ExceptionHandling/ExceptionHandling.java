875d10fda8eb387b924804f5c1b8b016e1fbb91a
Pradeep <pradeepparit2001@gmail.com>
2025-11-22 14:39:26 +0530

    Exception Handling Example

---
+++ Java/src/Pradeep/ExceptionHandling/ExceptionHandling.java
@@ -0,0 +1,33 @@
+package Pradeep.ExceptionHandling;
+
+import java.io.IOException;
+
+public class ExceptionHandling {
+    //    int test(){
+//        try{
+//            return 10/0;
+//        }
+//        catch(Exception e){
+//            return e.getMessage();
+//        }
+//        finally{
+//            return 1;
+//        }
+//    }
+    public static void main(String[] args) {
+        try {
+            System.out.println(10 / 0);
+        } catch (Exception e) {
+//            System.out.println(e.getCause().getMessage());
+            System.out.println(e.getMessage());
+            e.getStackTrace(); // This statement will print first or last
+            // because of System.out and System.err are two different output streams
+//            Their order can sometimes mix depending on the system buffering.
+            System.out.println("Caught Exception");
+            System.out.println(e.toString() + "abc");
+//            e = new IOException(); // We can create a new object of the another exception and throw it also but for that we should have to declare main method throws
+            throw e; // Here we can use sout also and also when we create a new object like above line then it will print null
+//            System.out.println(e.getMessage()); // Cannot write any statement after throw keyword
+        }
+    }
+}
