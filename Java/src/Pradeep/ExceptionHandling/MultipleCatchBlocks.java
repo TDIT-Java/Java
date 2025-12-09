02864e4790da6b15a2e16f2ace103a4e069e2605
Pradeep <pradeepparit2001@gmail.com>
2025-12-09 17:58:10 +0530

    MultipleCatchBlocks example

---
+++ Java/src/Pradeep/ExceptionHandling/MultipleCatchBlocks.java
@@ -0,0 +1,27 @@
+package Pradeep.ExceptionHandling;
+
+import java.io.FileNotFoundException;
+
+public class MultipleCatchBlocks {
+    public static void main(String[] args) {
+        try {
+            String name = null;
+            int[] a = new int[3];
+            System.out.println(10 / 0);
+            System.out.println(name.length());
+            System.out.println(a[4]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+// RULE 1:
+// Parent exception always should be written at last, because if we write parent first
+// then child will be no use or unreachable
